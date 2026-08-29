d4a7bd60061efe9ca573af8ac7200f85df868993
Salha-T <67549062+Salha-T@users.noreply.github.com>
2026-08-30 02:36:28 +1000

    Refresh GitHub branch status cache

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- Required for Tailwind v4
+import tailwindcss from '@tailwindcss/vite' 
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // <-- Must be included here
+    tailwindcss(), 
   ],
 })
